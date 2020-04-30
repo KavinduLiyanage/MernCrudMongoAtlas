8d991b205611a74388fd6a59229aa0dee4a4ecae
Kavindu8956 <sandeepa8956@gmail.com>
2020-04-30 13:38:56 +0530

    create product component implemented

--- src/components/create-product.component.js
+++ src/components/create-product.component.js
@@ -1,10 +1,57 @@
 import React, {Component} from 'react';
 
 class CreateProductComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeProductName = this.onChangeProductName.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            productName: ''
+        }
+    }
+
+    onChangeProductName(e) {
+        this.setState({
+            productName: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const product = {
+            productName: this.state.productName
+        }
+
+        console.log(product);
+
+
+        this.setState({
+            productName: ''
+        })
+    }
+
     render() {
         return (
             <div>
-                <p>You are on the Create Product Component</p>
+                <h3>Add new Product</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Product Name: </label>
+                        <input  type="text"
+                                required
+                                className="form-control"
+                                value={this.state.productName}
+                                onChange={this.onChangeProductName}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Add Product" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         );
     }
