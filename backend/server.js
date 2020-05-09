363d945813ed5b41a90c3ec00ca45f3874f1080a
Kavindu8956 <sandeepa8956@gmail.com>
2020-05-09 22:38:03 +0530

    backend warning remove

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-
-
 const app = express();
 
 const PORT = 4000;
+const cors = require('cors');
+
+const mongoose = require('mongoose');
 const config = require('./database/DB.js');
+
 app.use(cors());
 app.use(express.json());
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, {useNewUrlParser: true}).then(
-    ()=> {console.log('Database is connected')},
-    err => {console.log('cannot connect to the database' + err)}
+mongoose.connect(config.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(
+    ()=> {
+        console.log('Database is connected')
+    },
+    err => {
+        console.log('cannot connect to the database' + err)
+    }
 );
 
 const discountsRouter = require('./routes/discounts');
